bdbaf9bec051165fdcce4c3c08bd5ddb7ee63e08
mabounassif <mahmoud.abounassif@gmail.com>
2018-02-26 12:30:43 -0500

    Initial Commit

---
+++ CocoaPods-Issue7461/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CocoaPods-Issue7461
+//
+//  Created by mahmoud on 2/26/18.
+//  Copyright © 2018 mahmoud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CocoaPods-Issue7461/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CocoaPods-Issue7461
+//
+//  Created by mahmoud on 2/26/18.
+//  Copyright © 2018 mahmoud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
